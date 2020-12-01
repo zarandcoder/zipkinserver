2fb7fe56211bb06c3dfd021fc5534f4e58fb619d
Vadim Zaripov <vadim@zaripov.website>
2020-12-01 04:52:36 +0100

    Initial commit

---
+++ src/main/java/website/zaripov/zipkinserver/ZipkinserverApplication.java
@@ -0,0 +1,13 @@
+package website.zaripov.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/website/zaripov/zipkinserver/ZipkinserverApplicationTests.java
@@ -0,0 +1,13 @@
+package website.zaripov.zipkinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
